c7ba6dd1711806ead2c25e9da13b600cc6add10a
thalytasousa <thal.y.ta@hotmail.com>
2019-03-21 12:01:51 -0300

    modificação da versão dos pacotes
    modificação da versão do "debug" ( de 2.2.0 para 2.6.9) e do "growl" (de 1.8.1 para 1.10.0)

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -1,32 +1,30 @@
 defmodule RumblWeb.UserController do
     use RumblWeb, :controller
     alias Rumbl.Repo
+    alias Rumbl.User
 
 
     def index(conn, _params) do
-      users = Repo.all(Rumbl.User)
+      users = Repo.all(User)
       render(conn, "index.html", users: users)
     end
 
     def show(conn, %{"id" => id}) do
-      user = Repo.get(Rumbl.User, id)
+      user = Repo.get(User, id)
       render(conn, "show.html", user: user)
     end
 
     def new(conn, _params) do
-      changeset = Rumbl.User.changeset(%Rumbl.User{})
+      changeset = User.changeset(%User{})
       render conn, "new.html", changeset: changeset
     end
 
     def create(conn, %{"user" => user_params}) do
-      changeset = Rumbl.User.changeset(%Rumbl.User{}, user_params)
-      case Repo.insert(changeset) do
-        {:ok, user} ->
-            conn
-            |> put_flash(:info, "#{user.name} criado!")
-            |> redirect(to: Routes.user_path(conn, :index))
-            {:error, changeset} ->
-                render(conn, "new.html", changeset: changeset)
-      end
+      changeset = User.changeset(%User{}, user_params)
+      {:ok, user} = Repo.insert(changeset)
+
+      conn
+      |> put_flash(:info, "#{user.name} created!")
+      |> redirect(to: Routes.user_path(conn, :index))
     end
 end
