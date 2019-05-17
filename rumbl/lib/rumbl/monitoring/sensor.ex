325fb9a4cad28df4a081d95fe072f9db03a71294
thalytasousa <thal.y.ta@hotmail.com>
2019-05-17 17:17:08 -0300

    Create sensor.ex

---
+++ rumbl/lib/rumbl/monitoring/sensor.ex
@@ -0,0 +1,19 @@
+defmodule Rumbl.Monitoring.Sensor do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "sensors" do
+    field :latitude, :string
+    field :longitude, :string
+    belongs_to :river, Rumbl.World.River
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(sensor, attrs) do
+    sensor
+    |> cast(attrs, [:latitude, :longitude])
+    |> validate_required([:latitude, :longitude])
+  end
+end
